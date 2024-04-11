be8ba627364a9e20637d50d895f6da903bda519b
laurent2997 <162601315+laurent2997@users.noreply.github.com>
2024-04-11 16:14:22 +0200

    add .tofixed

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ let marker = L.marker([lat,long]).addTo(map);
 marker.bindPopup(`
     <h2>Hey you! Zoom in and discover Christchurch</h2>
     <ul>
-        <li>Breite: ${lat} </li>
-        <li>Länge: ${long}</li>
+        <li>Breite: ${lat.toFixed(5)} </li>
+        <li>Länge: ${long.toFixed(5)} </li>
     </ul>
 `).openPopup();
 
