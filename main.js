ad14f5de74558e6e34df4dbb2d5c75d44740c7fe
laurent2997 <162601315+laurent2997@users.noreply.github.com>
2024-04-11 15:54:13 +0200

    var to let

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-var map = L.map('map').setView([-43.531111,172.636667], 11);
+let map = L.map('map').setView([-43.531111,172.636667], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-43.531111, 172.636667]).addTo(map);
+let marker = L.marker([-43.531111, 172.636667]).addTo(map);
 marker.bindPopup("<h2>Hey you! Zoom in and discover Christchurch</h2><br>I am a popup.").openPopup();
+
+
+
+
+
+
+
+
+
+
 
 //marker anzeigen
 console.log(marker);
